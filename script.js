022fdea49a4e566e536de7b471cc4fe9ad7c2470
tsuji-eiji <eiji.rock24@gmail.com>
2021-02-07 16:28:01 +0900

    挙動の修正

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let audFlg = true;
 let questions = new Array;
 let question = new Array;
 
+let ansFlg = true;
 let ans = '';
 let teltime = 61;
 
@@ -24,11 +25,13 @@ document.getElementById('namebtn').addEventListener('click', function () {
 
 function quizMode() {
 	document.getElementById('quiz').style.visibility = 'visible';
+	document.getElementById('lifeline').style.visibility = 'visible';
 	document.getElementById('center').style.visibility = 'hidden';
 };
 
 function msgMode() {
 	document.getElementById('quiz').style.visibility = 'hidden';
+	document.getElementById('lifeline').style.visibility = 'hidden';
 	document.getElementById('center').style.visibility = 'visible';
 };
 
@@ -37,10 +40,8 @@ function init() {
 	questions = getCsv();
 	//ランダムに並び替え
 	questions = shuffle(questions);
-
 	//ループしながら問題を選択
 	question = makeQuestion(questions);
-
 }
 
 
@@ -49,6 +50,7 @@ function makeQuestion(questions) {
 	resetAns();
 	document.getElementById('audience').style.visibility = 'hidden';
 	num++;
+	ansFlg = true;
 	for (let item of questions) {
 		if (num === item[0]) {
 			document.getElementById('ask').textContent = `Q.${num}:${item[1]}`;
@@ -88,68 +90,76 @@ function shuffle(array) {
 	return array;
 }
 
-////回答を選択したときの処理
+//回答を選択したときの処理
 const ansArray = document.getElementsByClassName('select');
 for (let item of ansArray) {
 	item.addEventListener('click', function (e) {
-		resetAns();
-		ans = e.target.id;
-		coloredAns(ans, '#FDAE1D');
-		const selectedAns = document.getElementById(ans).textContent;
-		document.getElementById('center-content').innerHTML = `<p>${selectedAns}、ファイナルアンサー？？</p><button id="yesbtn" class="btn">はい</button><button id="nobtn" class="btn">いいえ</button>`;
-		document.getElementById('center').style.visibility = 'visible';
-		//ファイナルアンサーでいいえを選んだとき
-		document.getElementById('nobtn').addEventListener('click', function () {
-			document.getElementById('center').style.visibility = 'hidden';
+		if (ansFlg) {
 			resetAns();
-		});
-		//ファイナルアンサーではいを選んだとき
-		document.getElementById('yesbtn').addEventListener('click', function () {
-			const waitTime = 1000;
-			switch (ans) {
-				case 'a':
-					if (question[6] === 1) {
-						setTimeout(clear, waitTime);
-					} else {
-						setTimeout(failed, waitTime);
-					}
-					break;
-				case 'b':
-					if (question[6] === 2) {
-						setTimeout(clear, waitTime);
-					} else {
-						setTimeout(failed, waitTime);
-					}
-					break;
-				case 'c':
-					if (question[6] === 3) {
-						setTimeout(clear, waitTime);
-					} else {
-						setTimeout(failed, waitTime);
-					}
-					break;
-				case 'd':
-					if (question[6] === 4) {
-						setTimeout(clear, waitTime);
-					} else {
-						setTimeout(failed, waitTime);
-					}
-					break;
-			}
-		});
+			ans = e.target.id;
+			coloredAns(ans, '#FDAE1D');
+			ansFlg = false;
+			const selectedAns = document.getElementById(ans).textContent;
+			document.getElementById('center-content').innerHTML = `<p>${selectedAns}、ファイナルアンサー？？</p><button id="yesbtn" class="btn">はい</button><button id="nobtn" class="btn">いいえ</button>`;
+			document.getElementById('center').style.visibility = 'visible';
+			//ファイナルアンサーではいを選んだとき
+			document.getElementById('yesbtn').addEventListener('click', function () {
+				ansFlg = false;
+				const waitTime = 1000;
+				switch (ans) {
+					case 'a':
+						if (question[6] === 1) {
+							setTimeout(clear, waitTime);
+						} else {
+							setTimeout(failed, waitTime);
+						}
+						break;
+					case 'b':
+						if (question[6] === 2) {
+							setTimeout(clear, waitTime);
+						} else {
+							setTimeout(failed, waitTime);
+						}
+						break;
+					case 'c':
+						if (question[6] === 3) {
+							setTimeout(clear, waitTime);
+						} else {
+							setTimeout(failed, waitTime);
+						}
+						break;
+					case 'd':
+						if (question[6] === 4) {
+							setTimeout(clear, waitTime);
+						} else {
+							setTimeout(failed, waitTime);
+						}
+						break;
+				}
+			});
+			//ファイナルアンサーでいいえを選んだとき
+			document.getElementById('nobtn').addEventListener('click', function () {
+				ansFlg = true;
+				document.getElementById('center').style.visibility = 'hidden';
+				resetAns();
+			});
+		}
 	})
 };
 
 //回答が正解だったとき
 function clear() {
-	document.getElementById('quiz').style.visibility = 'hidden';
 	document.getElementById('center-content').innerHTML = `<p>正解！！</p><button id="nextbtn" class="btn">次へ</button>`;
+	coloredAns(ans, '#92CC41');
 	document.getElementById('nextbtn').addEventListener('click', function () {
 		if (num < 5) {
 			price *= 10;
 			question = makeQuestion(questions);
 			quizMode();
 		} else {
+			fiftyFlg = false;
+			telFlg = false;
+			audFlg = false;
 			document.getElementById('center-content').innerHTML = `<p>ミリオネア達成おめでとう！！</p><button id="nextbtn" class="btn">次へ</button>`;
 			document.getElementById('nextbtn').addEventListener('click', function () {
 				retry();
@@ -161,7 +171,11 @@ function clear() {
 
 //回答が不正解だったとき
 function failed() {
-	document.getElementById('quiz').style.visibility = 'hidden';
+	fiftyFlg = false;
+	telFlg = false;
+	audFlg = false;
+	ansFlg = false;
+	checkAns();
 	document.getElementById('center-content').innerHTML = `<p>残念！！</p><button id="nextbtn" class="btn">次へ</button>`;
 	document.getElementById('nextbtn').addEventListener('click', function () {
 		setTimeout(retry, 1000);
@@ -186,6 +200,7 @@ function retry() {
 	})
 	//再挑戦しない場合
 	document.getElementById('nobtn').addEventListener('click', function () {
+		document.getElementById('header').style.visibility = 'hidden';
 		document.getElementById('center-content').innerHTML = `<p>お疲れさまでした。<br>再挑戦する場合はページを再読み込みしてください。</p>`;
 	})
 }
@@ -227,37 +242,42 @@ function resetAns() {
 document.getElementById('fifty-fifty').addEventListener('click',
 	function () {
 		if (fiftyFlg) {
-			document.getElementById('center').style.visibility = 'visible';
-			document.getElementById('center-content').innerHTML = `<p>50:50を使用しますか？？</p><button id="yesbtn" class="btn">はい</button><button id="nobtn" class="btn">いいえ</button>`;
-			//使用する場合
-			document.getElementById('yesbtn').addEventListener('click', function () {
-				fiftyFlg = false;
-				let arrayAnswer = '';
-				switch (question[6]) {
-					case 1:
-						arrayAnswer = ['b', 'c', 'd'];
-						elaseAnswer(arrayAnswer);
-						break;
-					case 2:
-						arrayAnswer = ['a', 'c', 'd'];
-						elaseAnswer(arrayAnswer);
-						break;
-					case 3:
-						arrayAnswer = ['a', 'b', 'd'];
-						elaseAnswer(arrayAnswer);
-						break;
-					case 4:
-						arrayAnswer = ['a', 'b', 'c'];
-						elaseAnswer(arrayAnswer);
-						break;
-				}
-				document.getElementById('usedFifty').style.visibility = 'visible';
-				document.getElementById('center').style.visibility = 'hidden';
-			})
-			//使用しない場合
-			document.getElementById('nobtn').addEventListener('click', function () {
-				document.getElementById('center').style.visibility = 'hidden';
-			})
+			if (ansFlg) {
+				document.getElementById('center').style.visibility = 'visible';
+				ansFlg = false;
+				document.getElementById('center-content').innerHTML = `<p>50:50を使用しますか？？</p><button id="yesbtn" class="btn">はい</button><button id="nobtn" class="btn">いいえ</button>`;
+				//使用する場合
+				document.getElementById('yesbtn').addEventListener('click', function () {
+					fiftyFlg = false;
+					let arrayAnswer = '';
+					switch (question[6]) {
+						case 1:
+							arrayAnswer = ['b', 'c', 'd'];
+							elaseAnswer(arrayAnswer);
+							break;
+						case 2:
+							arrayAnswer = ['a', 'c', 'd'];
+							elaseAnswer(arrayAnswer);
+							break;
+						case 3:
+							arrayAnswer = ['a', 'b', 'd'];
+							elaseAnswer(arrayAnswer);
+							break;
+						case 4:
+							arrayAnswer = ['a', 'b', 'c'];
+							elaseAnswer(arrayAnswer);
+							break;
+					}
+					document.getElementById('usedFifty').style.visibility = 'visible';
+					document.getElementById('center').style.visibility = 'hidden';
+					ansFlg = true;
+				})
+				//使用しない場合
+				document.getElementById('nobtn').addEventListener('click', function () {
+					document.getElementById('center').style.visibility = 'hidden';
+					ansFlg = true;
+				})
+			}
 		}
 	}, false
 );
@@ -273,27 +293,32 @@ function elaseAnswer(arrayAnswer) {
 document.getElementById('telephone').addEventListener('click',
 	function () {
 		if (telFlg) {
-			document.getElementById('center').style.visibility = 'visible';
-			document.getElementById('center-content').innerHTML = `<p>テレフォンを使用しますか？？</p><button id="yesbtn" class="btn">はい</button><button id="nobtn" class="btn">いいえ</button>`;
-			//使用する場合
-			document.getElementById('yesbtn').addEventListener('click', function () {
-				document.getElementById('center-content').innerHTML = `<p>このボタンをクリックするとテレフォン開始です。</p><button id="startbtn" class="btn">START</button>`;
-				document.getElementById('startbtn').addEventListener('click', function () {
-					telFlg = false;
-					let countdown = setInterval(function () {
-						reculc();
-						if (teltime < 0) {
-							clearInterval(countdown);
-							document.getElementById('usedTel').style.visibility = 'visible';
-							document.getElementById('center').style.visibility = 'hidden';
-						}
-					}, 1000);
+			if (ansFlg) {
+				ansFlg = false;
+				document.getElementById('center').style.visibility = 'visible';
+				document.getElementById('center-content').innerHTML = `<p>テレフォンを使用しますか？？</p><button id="yesbtn" class="btn">はい</button><button id="nobtn" class="btn">いいえ</button>`;
+				//使用する場合
+				document.getElementById('yesbtn').addEventListener('click', function () {
+					document.getElementById('center-content').innerHTML = `<p>このボタンをクリックするとテレフォン開始です。</p><button id="startbtn" class="btn">START</button>`;
+					document.getElementById('startbtn').addEventListener('click', function () {
+						telFlg = false;
+						let countdown = setInterval(function () {
+							reculc();
+							if (teltime < 0) {
+								clearInterval(countdown);
+								document.getElementById('usedTel').style.visibility = 'visible';
+								document.getElementById('center').style.visibility = 'hidden';
+								ansFlg = true;
+							}
+						}, 1000);
+					})
 				})
-			})
-			//使用しない場合
-			document.getElementById('nobtn').addEventListener('click', function () {
-				document.getElementById('center').style.visibility = 'hidden';
-			})
+				//使用しない場合
+				document.getElementById('nobtn').addEventListener('click', function () {
+					document.getElementById('center').style.visibility = 'hidden';
+					ansFlg = true;
+				})
+			}
 		}
 	}, false
 );
@@ -308,62 +333,67 @@ function reculc() {
 document.getElementById('audienc').addEventListener('click',
 	function () {
 		if (audFlg) {
-			document.getElementById('center').style.visibility = 'visible';
-			document.getElementById('center-content').innerHTML = `<p>オーディエンスを使用しますか？？</p><button id="yesbtn" class="btn">はい</button><button id="nobtn" class="btn">いいえ</button>`;
-			//使用する場合
-			document.getElementById('yesbtn').addEventListener('click',()=>{
-				audFlg = false;
-				const audArray = [200, 150, 100, 50, 0];
-				let a = 0;
-				let b = 0;
-				let c = 0;
-				let d = 0;
-				document.getElementById('audience').style.visibility = 'visible';
-				if (document.getElementById('a').textContent !== '　') {
-					a = Math.floor(Math.random() * 100);
-				}
-				if (document.getElementById('b').textContent !== '　') {
-					b = Math.floor(Math.random() * 100);
-				}
-				if (document.getElementById('c').textContent !== '　') {
-					c = Math.floor(Math.random() * 100);
-				}
-				if (document.getElementById('d').textContent !== '　') {
-					d = Math.floor(Math.random() * 100);
-				}
-				switch (question[6]) {
-					case 1:
-						a += audArray[num - 1];
-						break;
-					case 2:
-						b += audArray[num - 1];
-						break;
-					case 3:
-						c += audArray[num - 1];
-						break;
-					case 4:
-						d += audArray[num - 1];
-						break;
-				}
-				const total = a + b + c + d;
-				const per_a = Math.round(a / total * 100);
-				const per_b = Math.round(b / total * 100);
-				const per_c = Math.round(c / total * 100);
-				const per_d = Math.round(d / total * 100);
-				document.getElementById('bar1').style.height = per_a * 2.5 + 'px';
-				document.getElementById('bar2').style.height = per_b * 2.5 + 'px';
-				document.getElementById('bar3').style.height = per_c * 2.5 + 'px';
-				document.getElementById('bar4').style.height = per_d * 2.5 + 'px';
-				document.getElementById('answer1').textContent = 'A.' + per_a + '%';
-				document.getElementById('answer2').textContent = 'B.' + per_b + '%';
-				document.getElementById('answer3').textContent = 'C.' + per_c + '%';
-				document.getElementById('answer4').textContent = 'D.' + per_d + '%';
-				document.getElementById('usedAud').style.visibility = 'visible';
-				document.getElementById('center').style.visibility = 'hidden';
-			})
-			document.getElementById('nobtn').addEventListener('click', function () {
-				document.getElementById('center').style.visibility = 'hidden';
-			})
+			if (ansFlg) {
+				ansFlg = false;
+				document.getElementById('center').style.visibility = 'visible';
+				document.getElementById('center-content').innerHTML = `<p>オーディエンスを使用しますか？？</p><button id="yesbtn" class="btn">はい</button><button id="nobtn" class="btn">いいえ</button>`;
+				//使用する場合
+				document.getElementById('yesbtn').addEventListener('click', () => {
+					audFlg = false;
+					const audArray = [200, 150, 100, 50, 0];
+					let a = 0;
+					let b = 0;
+					let c = 0;
+					let d = 0;
+					document.getElementById('audience').style.visibility = 'visible';
+					if (document.getElementById('a').textContent !== '　') {
+						a = Math.floor(Math.random() * 100);
+					}
+					if (document.getElementById('b').textContent !== '　') {
+						b = Math.floor(Math.random() * 100);
+					}
+					if (document.getElementById('c').textContent !== '　') {
+						c = Math.floor(Math.random() * 100);
+					}
+					if (document.getElementById('d').textContent !== '　') {
+						d = Math.floor(Math.random() * 100);
+					}
+					switch (question[6]) {
+						case 1:
+							a += audArray[num - 1];
+							break;
+						case 2:
+							b += audArray[num - 1];
+							break;
+						case 3:
+							c += audArray[num - 1];
+							break;
+						case 4:
+							d += audArray[num - 1];
+							break;
+					}
+					const total = a + b + c + d;
+					const per_a = Math.round(a / total * 100);
+					const per_b = Math.round(b / total * 100);
+					const per_c = Math.round(c / total * 100);
+					const per_d = Math.round(d / total * 100);
+					document.getElementById('bar1').style.height = per_a * 2.5 + 'px';
+					document.getElementById('bar2').style.height = per_b * 2.5 + 'px';
+					document.getElementById('bar3').style.height = per_c * 2.5 + 'px';
+					document.getElementById('bar4').style.height = per_d * 2.5 + 'px';
+					document.getElementById('answer1').textContent = 'A.' + per_a + '%';
+					document.getElementById('answer2').textContent = 'B.' + per_b + '%';
+					document.getElementById('answer3').textContent = 'C.' + per_c + '%';
+					document.getElementById('answer4').textContent = 'D.' + per_d + '%';
+					document.getElementById('usedAud').style.visibility = 'visible';
+					document.getElementById('center').style.visibility = 'hidden';
+					ansFlg = true;
+				})
+				document.getElementById('nobtn').addEventListener('click', function () {
+					document.getElementById('center').style.visibility = 'hidden';
+					ansFlg = true;
+				})
+			}
 		}
 	}, false
 );
